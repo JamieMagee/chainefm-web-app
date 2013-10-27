6c4a803bfd4c1139a4a101ff2dd75a1f73d87136
JamieMagee <jamie.magee@gmail.com>
2013-10-27 20:46:03 +0000

    Update to FontAwesome 4.0.0

--- js/audio.js
+++ js/audio.js
@@ -10,16 +10,16 @@ $(document).ready(function () {
       var value = $('#volume').slider('value');
       $('#radio')[0].volume = (value / 100);
       if (value >= 75) {
-        $('#volume-icon').html('<i class="icon-volume-up icon-2x"></i>')
+        $('#volume-icon').html('<i class="fa fa-volume-up fa-2x"></i>')
       }
       else if (value >= 25 && value < 75) {
-        $('#volume-icon').html('<i class="icon-volume-down icon-2x"></i>')
+        $('#volume-icon').html('<i class="fa fa-volume-down fa-2x"></i>')
       }
       else if (value > 1 && value < 25) {
-        $('#volume-icon').html('<i class="icon-volume-off icon-2x"></i>')
+        $('#volume-icon').html('<i class="fa fa-volume-off fa-2x"></i>')
       }
       else {
-        $('#volume-icon').html('<span class="icon-stack"><i class="icon-volume-off icon-stack-base"></i><i class="icon-ban-circle icon-stack-base text-error"></i></span>')
+        $('#volume-icon').html('<span class="fa-stack"><i class="fa fa-volume-off fa-stack-1x fa-2x"></i><i class="fa fa-ban fa-stack-2x text-error"></i></span>')
       }
     }
   });
@@ -27,15 +27,15 @@ $(document).ready(function () {
   $('#volume').draggable();
 
   $('#playpause').click(function () {
-    if (!$('#playpause').hasClass('icon-refresh')) {
+    if (!$('#playpause').hasClass('fa-refresh')) {
       if ($('#radio')[0].paused == false) {
-        $('#playpause').removeClass('icon-pause').addClass('icon-play');
+        $('#playpause').removeClass('fa-pause').addClass('fa-play');
         $('#radio')[0].pause();
         if ($('title').html().indexOf('▶ ') != -1) {
           $('title').text($('title').html().substring(2, $('title').html().length));
         }    
       } else {
-        $('#playpause').removeClass('icon-play').addClass('icon-pause');
+        $('#playpause').removeClass('fa-play').addClass('fa-pause');
         $('#radio')[0].play();
         $('title').prepend('▶ ');
       }
@@ -46,21 +46,21 @@ $(document).ready(function () {
     if ($('#radio')[0].volume > .01) {
       $('#radio')[0].volume = 0;
       $('#volume').slider('value', 0);
-      $('#volume-icon').html('<span class="icon-stack"><i class="icon-volume-off icon-stack-base"></i><i class="icon-ban-circle icon-stack-base text-error"></i></span>')
+      $('#volume-icon').html('<span class="fa-stack"><i class="fa fa-volume-off fa-stack-1x fa-2x"></i><i class="fa fa-ban fa-stack-2x text-error"></i></span>')
     }
     else {
       $('#radio')[0].volume = .8;
       $('#volume').slider('value', 80);
-      $('#volume-icon').html('<i class="icon-volume-up icon-2x"></i>')
+      $('#volume-icon').html('<i class="fa fa-volume-up fa-2x"></i>')
     }
   });
   
   $("#radio").on("canplaythrough", function(){
-    $('#playpause').attr( "class", "icon-play icon-2x");
+    $('#playpause').attr( "class", "fa fa-play fa-2x");
   });
   
   $("#radio").on("stalled", function(){
-    $('#playpause').attr( "class", "icon-refresh icon-spin icon-2x");
+    $('#playpause').attr( "class", "fa fa-refresh fa-spin fa-2x");
     if ($('title').html().indexOf('▶ ') != -1) {
       $('title').text($('title').html().substring(2, $('title').html().length));
     } 
