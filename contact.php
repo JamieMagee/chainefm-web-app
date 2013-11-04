36ff6bd75213b107314c2dc4d2fdc18e265cae60
JamieMagee <jamie.magee@gmail.com>
2013-11-04 00:29:37 +0000

    Don't need contact.js anymore

--- contact.php
+++ contact.php
@@ -112,7 +112,6 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="js/contact.js"></script>
     <script src="js/hover.js"></script>
     <script src="js/ga.js"></script>    
   </body>
